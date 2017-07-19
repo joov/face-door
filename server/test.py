fdce6b365ad8c9c24e4732161f2be578863c5507
joov <johannes.wenzel@web.de>
2017-07-19 23:49:51 +0200

    syb

--- server/test.py
+++ server/test.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/<path:path>', methods=['POST'])
 def catch_all(path):
         print("Path: {}".format(path))
-        print("Request Body: {}".format(request.body.raw))
+        print("Request Body: {}".format(request.get_json))
         return 'true'
 
 
