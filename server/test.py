5f89207a4c44194c51e18af2ae819319e029a2c7
joov <johannes.wenzel@web.de>
2017-07-19 22:12:34 +0200

    test

---
+++ server/test.py
@@ -0,0 +1,13 @@
+import json
+from flask import Flask, jsonify, request, redirect
+
+@app = Flask(__name__)
+
+@app.route('/',methods=['POST'])
+def post():
+        data = request.data
+        json.dumps(data)
+        return 'true'
+
+
+app.run(host='0.0.0.0', port=80)
