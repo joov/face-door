e0066ce84725ab97e90a36228b008c3abba2b209
Johannes Wenzel <johannes.wenzel@web.de>
2018-01-09 22:06:21 +0100

    print all lines

--- server/test_hough_fast.py
+++ server/test_hough_fast.py
@@ -8,7 +8,8 @@ edges = cv2.Canny(gray,50,100,apertureSize = 3)
 minLineLength = 100
 maxLineGap = 10
 lines = cv2.HoughLinesP(edges,1,np.pi/180,100,minLineLength,maxLineGap)
-for x1,y1,x2,y2 in lines[0]:
-    cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
+for line in lines:
+	for x1,y1,x2,y2 in line:
+		cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
 
 cv2.imwrite(sys.argv[1][:sys.argv[1].find('.jpg')]+'_w_lines_fast.jpg' ,img)
