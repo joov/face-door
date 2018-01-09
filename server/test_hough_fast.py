f49fed390024e011a5d0745978e96b70dfe98ce8
Johannes Wenzel <johannes.wenzel@web.de>
2018-01-09 22:14:05 +0100

    longer lines for fast hough

--- server/test_hough_fast.py
+++ server/test_hough_fast.py
@@ -7,7 +7,7 @@ gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 edges = cv2.Canny(gray,50,100,apertureSize = 3)
 minLineLength = 500
 maxLineGap = 5
-lines = cv2.HoughLinesP(edges,1,np.pi/180,50,minLineLength,maxLineGap)
+lines = cv2.HoughLinesP(edges,1,np.pi/180,10,minLineLength,maxLineGap)
 for line in lines:
 	for x1,y1,x2,y2 in line:
 		cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
