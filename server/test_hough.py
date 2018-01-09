05b58b8aa14f207bf11de591847a03ac7919a628
Johannes Wenzel <johannes.wenzel@web.de>
2018-01-09 22:04:44 +0100

    print all lines

--- server/test_hough.py
+++ server/test_hough.py
@@ -10,17 +10,18 @@ cv2.imwrite(sys.argv[1][:sys.argv[1].find('.jpg')]+'_edges.jpg' ,edges)
 
 lines = cv2.HoughLines(edges,2,np.pi/180,100)
 
-print('lines found ', len(lines[0]))
-for rho,theta in lines[0]:
-    a = np.cos(theta)
-    b = np.sin(theta)
-    x0 = a*rho
-    y0 = b*rho
-    x1 = int(x0 + 1000*(-b))
-    y1 = int(y0 + 1000*(a))
-    x2 = int(x0 - 1000*(-b))
-    y2 = int(y0 - 1000*(a))
+print('lines found ', len(lines))
+for line in lines:
+	for rho,theta in line:
+		a = np.cos(theta)
+		b = np.sin(theta)
+		x0 = a*rho
+		y0 = b*rho
+		x1 = int(x0 + 1000*(-b))
+		y1 = int(y0 + 1000*(a))
+		x2 = int(x0 - 1000*(-b))
+		y2 = int(y0 - 1000*(a))
 
-    cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
+		cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
 
 cv2.imwrite(sys.argv[1][:sys.argv[1].find('.jpg')]+'_w_lines.jpg' ,img)
