d066b61ab8f79b52a543dc741f227d447bd1b857
joov <johannes.wenzel@web.de>
2018-01-21 18:26:53 +0100

    syn

--- ansible/roles/pi/files/10-autocopy.sh
+++ ansible/roles/pi/files/10-autocopy.sh
@@ -1,6 +1,6 @@
-FILE=wpa_supplicant.conf
-SRC_DIR="$UM_MOUNTPOINT"
-DEST_DIR="/etc/wpa_supplicant"
+export FILE=wpa_supplicant.conf
+export SRC_DIR="$UM_MOUNTPOINT"
+export DEST_DIR="/etc/wpa_supplicant"
 if [[ -r "$SRC_DIR/$FILE" && -f "$SRC_DIR/$FILE" ]]; then
     cp "$SRC_DIR/$FILE" "$DEST_DIR"
     chmod 0600 "$DEST_DIR/$FILE"
